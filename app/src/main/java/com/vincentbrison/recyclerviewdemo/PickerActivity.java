b98c6ae1dd99ff1079400a0ffb03e269b0498840
Vincent Brison <vincent.brison@gmail.com>
2016-10-12 23:26:14 +0200

    Initial import

---
+++ app/src/main/java/com/vincentbrison/recyclerviewdemo/PickerActivity.java
@@ -0,0 +1,13 @@
+package com.vincentbrison.recyclerviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picker);
+    }
+}
